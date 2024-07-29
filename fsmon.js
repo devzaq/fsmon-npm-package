bf02563467679e5d081491230b7948c8ecfc0f82
Ankit Jha <aajjha9@gmail.com>
2024-07-30 02:11:19 +0530

    clean working project

---
+++ fsmon.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+import { spawn } from 'child_process'
+import { watch } from 'fs/promises'
+import { dirname } from 'path'
+
+const [node, _, file] = process.argv
+
+const spawnNode = () => {
+    const childProcess = spawn(node, [file])
+    childProcess.stdout.pipe(process.stdout)
+    childProcess.stderr.pipe(process.stderr)
+    childProcess.on('close', (code) => {
+        if (code !== null) {
+            process.exit(code)
+        }
+    })
+
+    return childProcess
+}
+
+let childProcess = spawnNode()
+const watcher = watch(dirname(file), { recursive: true })
+for await (const event of watcher) {
+    if (
+        !event.filename.includes('node_modules') &&
+        event.filename.endsWith('.js')
+    ) {
+        console.log("Restarting the server.🚀🚀")
+        childProcess.kill('SIGKILL')
+        childProcess = spawnNode()
+    }
+}
